5059a53f4223ca0c6364a606fb662d49803b50d3
David Thomas Hume <dth@dthu.me>
2014-12-05 13:26:27 +0000

    Prepare 0.1.0 release, again

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject org.dthume/java.io-bridge "0.1.0"
   :description "Java bridge for implementing input / output streams without proxy"
+  :url "http://github.com/dthume/java.io-bridge"
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :scm {:name "git"
+        :url "github.com/dthume/java.io-bridge"}
+  
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
